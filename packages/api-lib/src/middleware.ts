63e41a06b6e009d8d9fb4dec1182965a853b70d8
Théo Posty <theo@posty.fr>
2023-01-09 16:51:15 +0100

    add middleware blueprint and authentication Dto

---
+++ packages/api-lib/src/middleware.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express'
+
+export abstract class Middleware {
+  constructor() {
+    this.execute = this.execute.bind(this)
+  }
+
+  public abstract execute(req: Request, res: Response, next: NextFunction): void
+}
+
+export class AuthDto {
+  constructor(
+    public readonly _id: string,
+    public readonly email: string,
+    public readonly role: string,
+  ) {}
+
+  public static from(payload: AuthDto) {
+    return new AuthDto(payload._id.valueOf(), payload.email.valueOf(), payload.role.valueOf())
+  }
+}
+
+export type AuthPayload<T = Record<string, unknown>> = {
+  authUser: AuthDto
+} & T
