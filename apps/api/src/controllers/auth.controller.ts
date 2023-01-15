2dc733378c656948a277ceae6330c6af1e0c743f
Théo Posty <theo@posty.fr>
2023-01-15 09:32:24 +0100

    add me route

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { ValidateRequest } from '@middlewares/request-validator.middleware'
 import { AuthenticationDto } from '@models/Auth/auth.dto'
-import { HttpResponse } from '@theo-coder/api-lib'
+import { AuthRequest, HttpResponse } from '@theo-coder/api-lib'
 import { inject } from 'inversify'
 import {
   controller,
@@ -8,10 +8,13 @@ import {
   requestBody,
   BaseHttpController,
   response,
+  httpGet,
+  request,
 } from 'inversify-express-utils'
 import { IUser } from '@models/User/user.model'
 import { Response } from 'express'
 import { AuthService } from '@services/auth.service'
+import { AuthMiddleware } from '@middlewares/auth.middleware'
 
 @controller('/auth')
 export class AuthController extends BaseHttpController {
@@ -30,6 +33,14 @@ export class AuthController extends BaseHttpController {
     const user = await this._authService.login(payload)
 
     const response = HttpResponse.success(user, 200)
+    res.status(response.statusCode).json(response)
+  }
+
+  @httpGet('/me', AuthMiddleware)
+  public async me(@request() req: AuthRequest, @response() res: Response) {
+    const me = await req.body.authUser
+
+    const response = HttpResponse.success(me, 200)
     res.status(response.statusCode).json(response)
   }
 }
